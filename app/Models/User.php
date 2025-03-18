5e70d0bacaa2691c1b8fcd720ea048a65730dac4
leandrin <leandrinsilva22@gmail.com>
2025-03-17 21:29:01 -0300

    Extendendo classe User para Illuminate\Foundation\Auth\User

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,7 +3,6 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Foundation\Auth\User as Authenticatable;
